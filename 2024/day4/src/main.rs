0adb730e4091bce038b99cd057ed682dbdeeb2f5
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2024-12-11 14:22:09 +0000

    completed day 4

--- 2024/day4/src/main.rs
+++ 2024/day4/src/main.rs
@@ -3,32 +3,77 @@ use std::fs;
 fn solve_task_1(input: &str) -> i32 {
     let symbols: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     let mut count = 0;
-    let xmas = "XMAS".chars()
-    let samx = xmas.reverse();
+    let xmas: Vec<char> = "XMAS".chars().collect();
+
+    let directions: [(i32, i32); 9] = [
+        // downwards
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        // horizontally
+        (0, -1),
+        (0, 0),
+        (0, 1),
+        // upwards
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
 
     for i in 0..symbols.len() {
         for j in 0..symbols[i].len() {
             if symbols[i][j] == 'X' {
-                // searches SAMX
-                if j >= 4 {
-                    count += (symbols[i][j..j - 4] == ['S', 'A', 'M', 'X']) as u32;
-                // searches XMAS
-                } else if j < symbols[i].len() - 4 {
-                    count += (symbols[i][j..j + 4] == ['X', 'M', 'A', 'S']) as u32;
-                // searches SAMX vertically
-                } else if i >= 4 {
-                    count +=
-                        (symbols[i..i - 4].iter().map(|c| c[j]) == ['S', 'A', 'M', 'X']) as u32;
+                for direction in directions {
+                    let (x, y) = direction;
+
+                    // checking if we have enough space to move
+                    if ((j as i32 + 3 * x) >= 0 && (j as i32 + 3 * x) < symbols[i].len() as i32)
+                        && ((i as i32 + 3 * y) >= 0 && (i as i32 + 3 * y) < symbols.len() as i32)
+                    {
+                        let mut current_char = 1;
+                        for l in 1..4 {
+                            if symbols[(i as i32 + l * y) as usize][(j as i32 + l * x) as usize]
+                                == xmas[current_char]
+                            {
+                                current_char += 1;
+                            } else {
+                                break;
+                            }
+                        }
+                        if current_char == 4 {
+                            count += 1;
+                        }
+                    }
                 }
             }
         }
     }
 
-    1
+    count
 }
 
 fn solve_task_2(input: &str) -> i32 {
-    0
+    let symbols: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let mut count = 0;
+
+    for i in 0..symbols.len() {
+        for j in 0..symbols[i].len() {
+            if symbols[i][j] == 'A'
+                && i >= 1
+                && j >= 1
+                && i < symbols.len() - 1
+                && j < symbols[i].len() - 1
+            {
+                count += (((symbols[i - 1][j - 1] == 'M' && symbols[i + 1][j + 1] == 'S')
+                    || (symbols[i - 1][j - 1] == 'S' && symbols[i + 1][j + 1] == 'M'))
+                    && ((symbols[i + 1][j - 1] == 'S' && symbols[i - 1][j + 1] == 'M')
+                        || (symbols[i + 1][j - 1] == 'M' && symbols[i - 1][j + 1] == 'S')))
+                    as i32;
+            }
+        }
+    }
+
+    count
 }
 
 fn main() {
